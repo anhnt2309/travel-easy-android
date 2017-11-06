018d517e2c780f01276a40203873bbdf96b99ffe
Tuan_Anh <anhnt.2309@gmail.com>
2017-11-06 16:42:07 +0700

    apimanager updated

--- app/src/main/java/com/httt/uit/travel_easy_android/manager/ApiManager.java
+++ app/src/main/java/com/httt/uit/travel_easy_android/manager/ApiManager.java
@@ -4,10 +4,12 @@ import android.content.Context;
 
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
+import com.httt.uit.travel_easy_android.model.AutoCompleteAirport;
 import com.httt.uit.travel_easy_android.request.BaseRequest;
 import com.httt.uit.travel_easy_android.request.MyDataCallback;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,4 +28,34 @@ public class ApiManager extends BaseRequest {
         String url = "location/" + locationCode;
         GET(context, url, null, type, null, myCallback);
     }
+
+    public static void getAutoCompleteAirport(final Context context, String text, String countryCode, MyDataCallback<ArrayList<AutoCompleteAirport>> myCallback) {
+        if (text == null || text.equals(""))
+            return;
+        HashMap<String, Object> params = new HashMap<>();
+        if (countryCode != null)
+            params.put("country", countryCode);
+        params.put("term", text);
+
+
+        Type type = new TypeToken<ArrayList<AutoCompleteAirport>>() {
+        }.getType();
+        String url = "airports/autocomplete";
+        GET(context, url, params, type, null, myCallback);
+    }
+
+    public static void getNearestAirports(final Context context, double lat, double lg, MyDataCallback<ArrayList<AutoCompleteAirport>> myCallback) {
+        if (lat == 0 || lg == 0)
+            return;
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("latitude", lat);
+        params.put("longitude", lg);
+
+
+        Type type = new TypeToken<ArrayList<AutoCompleteAirport>>() {
+        }.getType();
+        String url = "airports/nearest-relevant";
+        GET(context, url, params, type, null, myCallback);
+    }
+
 }
