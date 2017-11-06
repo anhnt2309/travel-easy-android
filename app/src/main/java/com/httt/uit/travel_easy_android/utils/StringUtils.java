bb57c7437e5e24d97c3b840e9a0772c47287395d
Tuan_Anh <anhnt.2309@gmail.com>
2017-11-06 16:38:51 +0700

    string utils updated

---
+++ app/src/main/java/com/httt/uit/travel_easy_android/utils/StringUtils.java
@@ -0,0 +1,31 @@
+package com.httt.uit.travel_easy_android.utils;
+
+import com.httt.uit.travel_easy_android.model.AutoCompleteAirport;
+
+/**
+ * Created by TuanAnh on 11/5/17.
+ */
+
+public class StringUtils {
+    public static AutoCompleteAirport parseAutoCompleteModel(AutoCompleteAirport model) {
+        if (model == null)
+            return null;
+        String label = model.label;
+        String cityName = "";
+        String aiportName = "";
+        if (!label.contains("-")) {
+            cityName = null;
+            aiportName = label.substring(0, label.indexOf("["));;
+            model.setHasCityName(false);
+        }
+        if (label.contains("-")) {
+            model.setHasCityName(true);
+            cityName = label.substring(0, label.indexOf("-"));
+            aiportName = label.substring(label.indexOf("-") + 1, label.indexOf("["));
+        }
+        model.setCityName(cityName);
+        model.setAirportName(aiportName);
+        return model;
+    }
+
+}
