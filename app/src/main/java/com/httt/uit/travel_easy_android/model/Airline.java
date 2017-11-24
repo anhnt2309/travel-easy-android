24388b4afff761069aa0129f1b3c1f4e4e067d37
Tuan_Anh <anhnt.2309@gmail.com>
2017-11-24 17:16:11 +0700

    model for crawling

---
+++ app/src/main/java/com/httt/uit/travel_easy_android/model/Airline.java
@@ -0,0 +1,19 @@
+package com.httt.uit.travel_easy_android.model;
+
+/**
+ * Created by TuanAnh on 11/23/17.
+ */
+
+public class Airline {
+    public String name;
+    public String website;
+    public String country;
+    public String countryImage;
+
+    public Airline(String name, String website, String country, String countryImage) {
+        this.name = name;
+        this.website = website;
+        this.country = country;
+        this.countryImage = countryImage;
+    }
+}
