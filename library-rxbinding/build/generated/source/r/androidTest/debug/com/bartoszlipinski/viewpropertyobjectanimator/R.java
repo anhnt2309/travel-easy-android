11d216869fa73974c3f79b3212f9d5363b8dfecd
Tuan_Anh <anhnt.2309@gmail.com>
2017-12-27 14:10:31 +0700

    zzz

--- library-rxbinding/build/generated/source/r/androidTest/debug/com/bartoszlipinski/viewpropertyobjectanimator/R.java
+++ library-rxbinding/build/generated/source/r/androidTest/debug/com/bartoszlipinski/viewpropertyobjectanimator/R.java
@@ -8,6 +8,6 @@ package com.bartoszlipinski.viewpropertyobjectanimator;
 
 public final class R {
     public static final class string {
-        public static final int app_name = 0x7f060021;
+        public static final int app_name = 0x7f0b001f;
     }
 }
--- library-rxbinding/build/generated/source/r/debug/com/bartoszlipinski/viewpropertyobjectanimator/R.java
+++ library-rxbinding/build/generated/source/r/debug/com/bartoszlipinski/viewpropertyobjectanimator/R.java
@@ -8,6 +8,6 @@ package com.bartoszlipinski.viewpropertyobjectanimator;
 
 public final class R {
     public static final class string {
-        public static int app_name = 0x7f060021;
+        public static int app_name = 0x7f150020;
     }
 }
